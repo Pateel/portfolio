1d65f8724eda5615fcd08a8d64165ecc027b8b25
Pateel <54644087+Pateel@users.noreply.github.com>
2023-04-17 19:13:15 -0400

    Add files via upload
    uploading project

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){  
+  $("p").hover(function(){  
+      $(this).css("background-color", "white");  
+      }, function(){  
+      $(this).css("background-color", "#F2F3F7");  
+  });  
+}); 
+
+$(document).ready(function(){  
+  $("p").hover(function(){  
+      $(this).css("background-color", "white");  
+      }, function(){  
+      $(this).css("background-color", "#F2F3F7");  
+  });  
+}); 
+
+
